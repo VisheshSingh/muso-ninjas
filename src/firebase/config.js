789b3a573ab9055a52405b7598b36ade0c64547e
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-31 16:59:08 -0600

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAq0-IhuCdYQtxf2VeZdUSzVrjWMYLMXoU',
+  authDomain: 'muso-ninjas-ca649.firebaseapp.com',
+  projectId: 'muso-ninjas-ca649',
+  storageBucket: 'muso-ninjas-ca649.appspot.com',
+  messagingSenderId: '1034219156624',
+  appId: '1:1034219156624:web:52f7906d0d0851a9181415',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamps
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectAuth, projectFirestore, timestamp };
