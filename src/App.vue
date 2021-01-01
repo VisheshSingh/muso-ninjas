5d5dd0935d1b898b24988c520e4a3796c92c36c5
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-31 18:05:58 -0600

    navbar comp

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <router-view />
+  <Navbar />
+  <div class="content">
+    <router-view />
+  </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar';
+
+export default {
+  name: 'App',
+  components: { Navbar },
+};
+</script>
 
 <style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <h1><router-link :to="{ name: 'Home' }">Muso Ninjas</router-link></h1>
+      <div class="links">
+        <button class="btn">Logout</button>
+        <router-link :to="{ name: 'Signup' }" class="btn">Signup</router-link>
+        <router-link :to="{ name: 'Login' }" class="btn">Login</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style>
+.navbar {
+  background: #fff;
+  margin-bottom: 60px;
+  padding: 16px 10px;
+}
+
+nav {
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  align-content: center;
+}
+
+nav h1 {
+  margin-left: 20px;
+}
+
+nav .links {
+  margin-left: auto;
+}
+
+nav .links a,
+nav button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+</style>
