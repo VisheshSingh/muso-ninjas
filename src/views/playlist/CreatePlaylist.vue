162cfe9e54de746023a62bc626b83acb88550848
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-04 10:19:48 -0600

    adding new playlist

--- src/views/playlist/CreatePlaylist.vue
+++ src/views/playlist/CreatePlaylist.vue
@@ -10,27 +10,49 @@
     <label>Upload playlist cover image</label>
     <input type="file" @change="handleChange" />
     <div class="error" v-if="fileError">{{ fileError }}</div>
-    <button>Create</button>
+    <button v-if="!isPending">Create</button>
+    <button v-else>Saving...</button>
   </form>
 </template>
 
 <script>
 import { ref } from 'vue';
 import useStorage from '@/composables/useStorage';
+import useCollection from '@/composables/useCollection';
+import getUser from '@/composables/getUser';
+import { timestamp } from '@/firebase/config';
 
 export default {
   name: 'CreatePlaylist',
   setup() {
-    const { url, uploadImage } = useStorage();
+    const { url, filePath, uploadImage } = useStorage();
+    const { error, addDoc } = useCollection('playlist');
+    const { user } = getUser();
+
     const title = ref('');
     const description = ref('');
     const file = ref(null);
     const fileError = ref(null);
+    const isPending = ref(false);
 
     const handleSubmit = async () => {
       if (file.value) {
+        isPending.value = true;
         await uploadImage(file.value);
-        console.log('image uploaded url: ', url.value);
+        await addDoc({
+          title: title.value,
+          description: description.value,
+          userId: user.value.uid,
+          userName: user.value.displayName,
+          coverUrl: url.value,
+          filePath: filePath.value,
+          songs: [],
+          createdAt: timestamp(),
+        });
+        isPending.value = false;
+        if (!error.value) {
+          console.log('playlist added!');
+        }
       }
     };
 
@@ -47,7 +69,14 @@ export default {
       }
     };
 
-    return { title, description, handleSubmit, handleChange, fileError };
+    return {
+      title,
+      description,
+      handleSubmit,
+      handleChange,
+      fileError,
+      isPending,
+    };
   },
 };
 </script>
