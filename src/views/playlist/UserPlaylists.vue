8f9b90d0ac7d5b29ab12793695918cc783b2cbe1
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-05 10:53:56 -0600

    firestore queries

--- src/views/playlist/UserPlaylists.vue
+++ src/views/playlist/UserPlaylists.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="user-playlists"><h1>My playlists</h1></div>
+  <div class="user-playlists"><h2>My playlists</h2></div>
 </template>
 
 <script>
+import getUser from '@/composables/getUser';
+import getCollection from '@/composables/getCollection';
+
 export default {
   name: 'UserPlaylists',
+  setup() {
+    const { user } = getUser();
+    const { documents: playlists } = getCollection('playlist', [
+      'userId',
+      '==',
+      user.value.uid,
+    ]);
+
+    return { playlists };
+  },
 };
 </script>
 
