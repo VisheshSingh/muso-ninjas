32f1face0bfd24bbbefbfc925469038e9b8f93b9
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-05 10:59:40 -0600

    listing users playlists

--- src/views/playlist/UserPlaylists.vue
+++ src/views/playlist/UserPlaylists.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="user-playlists"><h2>My playlists</h2></div>
+  <div class="user-playlists">
+    <h2>My playlists</h2>
+    <div v-if="playlists">
+      <ListView :playlists="playlists" />
+    </div>
+    <router-link :to="{ name: 'CreatePlaylist' }" class="btn"
+      >Create Playlist</router-link
+    >
+  </div>
 </template>
 
 <script>
 import getUser from '@/composables/getUser';
 import getCollection from '@/composables/getCollection';
+import ListView from '@/components/ListView';
 
 export default {
   name: 'UserPlaylists',
+  components: { ListView },
   setup() {
     const { user } = getUser();
     const { documents: playlists } = getCollection('playlist', [
